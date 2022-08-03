dd5880d426171dd99346e78a6ed519b210a22c88
Radkevych Oksana <radkevych.oksana@gmail.com>
2022-08-03 16:09:44 +0200

    API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary(props) { 
-    /*Оголошуємо станд для тексту з форми ввода keyword, та зміну статусу після того як текст введться setKeyword, початковий статус пустий рядок,Відміняються дефолтні події в бразері*/  
+
+   /*оголошуємо keyword як "" (до вводу тексту в форму)*/  
   let [keyword, setKeyword] = useState("");
+
+  /*Функція handleResponse повертає обєкт через API з https://dictionaryapi.dev/*/
+   function handleResponse(response){
+    console.log(response);
+   } 
+
+   /*
+    1.функція search виводить значення з поля вводу в формі.
+    2. Підключає Api (https://dictionaryapi.dev/).
+  
+  */
   function search(event) {
     event.preventDefault();
-    alert(`My word is ${keyword}`);
+
+    let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(handleResponse);
+        
   }
+
+   /*функція handleKeywordChange змінює статус keyword "" на  event.target.valueз*/
   function handleKeywordChange(event) {
-    
-      /*знаходимо властивості в обєкті event, що передане з поля вводу, в яких лежить це значення, і змінюємо статус на setKeyword*/
-    
    setKeyword(event.target.value);
   }
+  /*
+  Події
+    1.При відправці форми подія  onSubmit запускає функцію search.  
+    2.При введенні тексту в поле вводу форми подія  onChange, що запускає функцію handleKeywordChange.
+  */
+
   return (
-    /*При відправці форми запускається функція search*/
-     /*Текст з поля вводу відправляємо на функцію handleKeywordChange*/
-    <div className="Dictionary">
+    <div className="Dictionary">  
       <form onSubmit={search}>
         <input
           type="search"

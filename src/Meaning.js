caf6c5f8227277866f53498d5b0a93a117c7016b
Radkevych Oksana <radkevych.oksana@gmail.com>
2022-08-04 20:49:00 +0200

    updated synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -13,22 +13,23 @@ import Example from "./Example";
   1.Synonyms з властивістю props.meaning.synonyms
 */
 export default function Meaning(props) {
- return (
-<div className="Meaning">
-    <h3>{props.meaning.partOfSpeech}</h3>
-    {props.meaning.definitions.map(function (definition, index) {
-      return (
-        <div key={index}>
-          <p>
-          <strong>Definition:</strong>
-          {definition.definition} <br/>
-          <Example example={definition.example}/>
-          <br/>
-          <Synonyms synonyms={props.meaning.synonyms}/>
-          </p>
-        </div>
-      );
-    })}
-  </div>
- );
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        console.log(definition.synonyms);
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition:</strong>
+              {definition.definition} <br />
+              <Example example={definition.example} />
+            </p>
+          </div>
+        );
+      })}
+      <strong>Synonyms:</strong>
+      <Synonyms synonyms={props.meaning.synonyms} />
+    </div>
+  );
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,16 +1,18 @@
 import React from "react";
 /* Умова виведення - якщо props.synonyms === true (є даніњ), виводимо блок з синонімами, або нічого (null) */
 export default function Synonyms(props) {
-  return (
-    <>
-     <strong>Synonym:</strong>
-      {props.synonyms.map(function (synonym, index) {
-        return (
-            <ul>
-              <li key={index}>{synonym}</li>
-            </ul>   
-        ); 
-      })}
-    </>
-  );
+  console.log(props.synonyms);
+  if (props.synonyms) {
+    return (
+      <div>
+        <ul className="Synonyms">
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
